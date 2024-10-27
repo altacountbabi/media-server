ec44a953962ca3a5a1d15e70ffa9fc84f95c09c4
altacountbabi <altacountbabi@users.noreply.github.com>
2024-10-27 16:26:49 +0200

    Add config back.

---
+++ data/config/default.nix
@@ -0,0 +1,13 @@
+{ lib, ... }:
+
+{
+  dataDir = "/home/real/projects/media-server/data";
+
+  libraries = [rec {
+    name = "Movies";
+    type = name;
+    folders = [ (lib.toLower name) ];
+  }];
+
+  apiKeys = import ./keys.nix;
+}
